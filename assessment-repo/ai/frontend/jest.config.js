761a6a14043c04cc8259bda60acbfcd8b1fc5658
Noly-17 <devnoly17@gmail.com>
2025-05-08 17:32:16 +0800

    remove unnecessary folders

--- assessment-repo/ai/frontend/jest.config.js
+++
@@ -1,6 +0,0 @@
-module.exports = {
-  setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
-  testEnvironment: 'jsdom',
-  testRegex: '(/__tests__/.*|(\\.|/)(test|spec))\\.[jt]sx?$',
-  moduleFileExtensions: ['js', 'jsx', 'ts', 'tsx', 'json'],
-};
